45a39bcc66721de79b64945e8b5d354146d7dd33
Timothy Raymond <tcraymond@inductivereasoning.com>
2024-12-27 00:05:32 -0500

    Revert "Let's try to make the DOF take a template parameter for the type of value associated with the DOF."
    This reverts commit 3e831663ff4dda9e0933ef89b973eb48bcb2b683.

--- TransFE/dof.h
+++ TransFE/dof.h
@@ -1,23 +1,11 @@
-/***************************************************************************
- *   Copyright (C) 2005-2024 by T. C. Raymond                              *
- *   tcraymond@inductivereasoning.com                                      *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                  *
- *                                                                         *
- ***************************************************************************/
+//************************************************************************
+//  			dof.h - Copyright T. C. Raymond
+//
+//**************************************************************************
 
-#pragma once
+#ifndef DOF_H
+#define DOF_H
 
-#include <cstddef>
-#include <type_traits>
-#include <complex>
-
- // Define the Complex type for convenience
-using Complex = std::complex<double>;
-
-// Define DOF status constants
 #define DOF_STATUS int
 #define DOF_Free 1
 #define DOF_Zero 2
@@ -25,48 +13,44 @@ using Complex = std::complex<double>;
 
 extern size_t ndof;
 
-/// This class represents a Degree of Freedom (DOF).
-template <typename ValueType>
+/// This class represents a Degree of Freedom (DOF). 
+
 class DOF {
-    // Restrict ValueType to double and Complex
-    static_assert(std::is_same_v<ValueType, double> || std::is_same_v<ValueType, Complex>,
-        "DOF only supports double or Complex as ValueType");
 
 public:
-    /// Default constructor
-    DOF() : value(0), status(DOF_Free), eq_number(0) {
-        ndof++;
-    }
 
-    /// Destructor
-    ~DOF() {
-        if (status == DOF_Free) {
-            ndof--;
-        }
-    }
+	///Default constructor
+	DOF() {
+		value = 0;
+		status = DOF_Free;
+		eq_number = 0;
+		ndof++;
+	}
 
-    /// Set the value
-    void set_value(const ValueType& val) { value = val; }
+	~DOF() {
+		if (status == DOF_Free) {
+			ndof--;
+		}
+	}
 
-    /// Get the value
-    ValueType get_value() const { return value; }
+	void set_value(double);
+	double get_value();
 
-    /// Set the status
-    void set_status(DOF_STATUS s) { status = s; }
+	void set_status(DOF_STATUS);
+	DOF_STATUS get_status();
 
-    /// Get the status
-    DOF_STATUS get_status() const { return status; }
+	void set_eqnumber(size_t);
+	size_t get_eqnumber();
 
-    /// Set the equation number
-    void set_eqnumber(size_t eq_num) { eq_number = eq_num; }
-
-    /// Get the equation number
-    size_t get_eqnumber() const { return eq_number; }
+protected:
 
 private:
-    ValueType value;    // The value of the DOF
-    DOF_STATUS status;  // The status of the DOF
-    size_t eq_number;   // The equation number
+
+	double value;
+	DOF_STATUS status;
+	size_t eq_number;  //the number of nodes could be limited by the size of this variable
+
 };
 
+#endif //DOF_H
 
